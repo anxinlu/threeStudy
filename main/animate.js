dd24449ff2d6c36bfa8840834d336f3bf6e069d4
luanxin <luanxin@pxsemic.com>
2023-06-20 15:45:13 +0800

    fix: 完成第一个动画

---
+++ main/animate.js
@@ -0,0 +1,38 @@
+import * as THREE from 'three'
+
+
+// 引入需要的工具
+// const { Scene,BoxGeometry,Mesh,MeshBasicMaterial,PerspectiveCamera } = THREE
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color:'skyblue'})
+const cube = new THREE.Mesh(geometry,material)
+cube.position.set(0,0,0)
+scene.add(cube)
+
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.5, 1000)
+
+camera.position.set(10,10,10)
+camera.lookAt(0,0,0)
+
+const renderer = new THREE.WebGLRenderer()
+
+renderer.setSize(window.innerWidth, window.innerHeight)  //先设置渲染器的大小之后再进行渲染
+
+document.body.appendChild(renderer.domElement)
+
+
+
+
+const animate = () => {
+    cube.rotation.x+= 0.01
+    cube.rotation.y+= 0.01
+    cube.rotation.z+= 0.01
+    renderer.render(scene, camera)
+
+    requestAnimationFrame(animate)
+}
+
+animate()
--- main/index.js
+++ main/index.js
@@ -1,41 +1,26 @@
 import * as THREE from 'three'
-console.log(THREE)
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-//创建场景
-const scene = new THREE.Scene()
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-//创建相机
-//创建正交透视相机  模拟人眼 根据几个参数配置视椎体
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.5,1000)
-camera.position.set(0,0,0)
-camera.lookAt(5, 5, 5)
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-/**
- * 创建物体的四个步骤
- * 1、创建几何体
- * 2、指定几何体的材质，也就是一些类似颜色、透明度等等之类的属性
- * 3、利用Mesh将几何体和材质组合
- * 4、将组合好后的物体添加到场景中去
- */
-const geometry = new THREE.BoxGeometry(1,1,1) //骨架
-const material = new THREE.MeshBasicMaterial({color:'red'}) //皮肤
-const cube = new THREE.Mesh(geometry,material) // 合体
-cube.position.set(1,1,1) 
-scene.add(cube) //上场
+camera.position.set(10,10,10)
+camera.lookAt(0,0,0)
 
+function animate() {
+    requestAnimationFrame( animate );
+  
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.005;
 
-/** 需要渲染器来渲染指定好的内容 */
-const render = new THREE.WebGLRenderer()
-render.setSize(window.innerWidth, window.innerHeight)
+    renderer.render( scene, camera );
+}
 
-render.render(scene,camera)
-
-/**
- * 将渲染器中的canvas指定的内容添加到body中
- */
-document.body.appendChild(render.domElement)
-
-console.log('tes')
-
-
-    
+animate()
