c0e4b89f94f2e9ff842bcce6af3abe22f5daa612
luanxin <luanxin@pxsemic.com>
2023-06-27 09:14:03 +0800

    fix: 设置轨道控制器阻尼系数

--- main/animate.js
+++ main/animate.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 
+// 导入轨道控制器
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+import gsap from 'gsap'
 
 
 // 引入需要的工具
@@ -25,24 +28,93 @@ renderer.setSize(window.innerWidth, window.innerHeight)  //先设置渲染器的
 
 document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
 
-const axesHelper = new THREE.AxesHelper(5)
+controls.enableDamping = true;    // 设置阻尼系数
+
+const axesHelper = new THREE.AxesHelper(15)
 scene.add(axesHelper)
-let scale = 1
-const animate = () => {
-    cube.rotation.x+= 0.01
-    cube.rotation.y+= 0.01
-    cube.rotation.z+= 0.01
-    
-    if(cube.position.x >= 5){
-        cube.position.x = 0
-        scale = 1
-    }else if(0< cube.position.x <5){
-        scale+= 0.005
-        cube.position.x+= 0.01
+// Clock 类可以用来 
+// const clock = new THREE.Clock()  
+
+gsap.to(
+    cube.position, 
+    {
+        x:5, 
+        duration:10,  // 动画执行的时长
+        repeat:-1,
+        yoyo:true,
+        onStart: () => {
+            console.log('动画开始')
+        },
+        onComplete:() => {
+            console.log('动画结束')
+        }
     }
-    cube.scale.set(scale,scale,scale)
+)
+
+gsap.to(
+    cube.rotation, 
+    {
+        x:2 * Math.PI, 
+        duration:10,  // 动画执行的时长
+        repeat:-1,
+        yoyo:true,
+        onStart: () => {
+            console.log('动画开始')
+        },
+        onComplete:() => {
+            console.log('动画结束')
+        }
+    }
+)
+
+
+gsap.to(
+    cube.position, 
+    {
+        y:5, 
+        duration:10,  // 动画执行的时长
+        repeat:-1,
+        yoyo:true,
+        onStart: () => {
+            console.log('动画开始')
+        },
+        onComplete:() => {
+            console.log('动画结束')
+        }
+    }
+)
+
+gsap.to(
+    cube.position, 
+    {
+        z:-50, 
+        duration:10,  // 动画执行的时长
+        repeat:-1,
+        yoyo:true,
+        onStart: () => {
+            console.log('动画开始')
+        },
+        onComplete:() => {
+            console.log('动画结束')
+        }
+    }
+)
+
+
+const animate = () => {
+    // cube.rotation.x+= 0.01
+    // cube.rotation.y+= 0.01
+    // cube.rotation.z+= 0.01
+    // const elapsedTime = clock.getElapsedTime()
+    // const offsetX = elapsedTime * 1   // 时间 * 单位时间挪动的像素
+    // cube.position.x = offsetX % 5
+    // 这个可以指定动画的轨迹以及运动的方式  及  渲染的时长
+
+    // 设置阻尼系数后需要再每一帧调用轨道控制器的update
+    controls.update()
+
     renderer.render(scene, camera)
     requestAnimationFrame(animate)
 }
